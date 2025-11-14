dadc07ea9ed8be9db13474be5ef23bd2dfb1208d
BSVS <uyv310599@gmail.com>
2025-11-13 22:27:05 -0600

    feat(structure): packages and controller classes have been added (no code)

--- src/java/oop/ecosystem/simulation/JavaOopEcosystemSimulation.java
+++ src/java/oop/ecosystem/simulation/JavaOopEcosystemSimulation.java
@@ -10,11 +10,9 @@ package java.oop.ecosystem.simulation;
  */
 public class JavaOopEcosystemSimulation {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
